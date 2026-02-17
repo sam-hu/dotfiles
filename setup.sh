5f25d2e2d105dcce4331ee1b48efb74114bff154
Sam Hu <hi@sam-hu.com>
2026-02-17 14:02:14 -0500

    reorder

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 # Get the directory where this script is located
 SCRIPT_DIR="$( cd "$( dirname "${(%):-%x}" )" && pwd )"
 
+"$SCRIPT_DIR/symlink.sh"
 "$SCRIPT_DIR/homebrew.sh"
 "$SCRIPT_DIR/clitools.sh"
-"$SCRIPT_DIR/symlink.sh"
